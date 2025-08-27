fdc94216cff14e4cb7291d19721e9d8ff3763c46
Pulkit Verma <pulkitverma1203@gmail.com>
2025-08-27 19:13:53 +0530

    Remove QR section from homepage for cleaner design

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,20 +1,18 @@
 import Header from "@/components/Header";
 import HeroSection from "@/components/HeroSection";
-import CafeGrid from "@/components/CafeGrid";
-import QRCodeSection from "@/components/QRCodeSection";
 import LoyaltyProgram from "@/components/LoyaltyProgram";
-
+import CafeGrid from "@/components/CafeGrid";
 
 const Index = () => {
   return (
-    <main className="min-h-screen">
+    <div className="min-h-screen bg-background">
       <Header />
-      <HeroSection />
-      <CafeGrid />
-      <QRCodeSection />
-      <LoyaltyProgram />
-
-    </main>
+      <main>
+        <HeroSection />
+        <LoyaltyProgram />
+        <CafeGrid />
+      </main>
+    </div>
   );
 };
 
